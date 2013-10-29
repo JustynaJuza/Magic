1d46e5ea65d223b6e961d480cc0a42fdfeadfd8d
Justyna <justyna@thetin.net>
2013-10-29 12:15:23 +0000

    Website styling fix

--- Magic/App_Start/BundleConfig.cs
+++ Magic/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Magic
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/_bootstrap.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
