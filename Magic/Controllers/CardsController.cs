9ef87f23321f7b41eda3ea672e06f9be341bfe1c
Justyna <justyna@thetin.net>
2014-07-14 12:39:18 +0100

    CMS controller updates

--- Magic/Controllers/CardsController.cs
+++ Magic/Controllers/CardsController.cs
@@ -30,19 +30,42 @@ namespace Magic.Controllers
         }
 
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            string errorText;
-            var model =  context.Read<Card, int>(id, out errorText);
-            TempData["Error"] = errorText;
+            if (id > 0)
+            {
+                string errorText;
+                var model = context.Read<Card, int>((int) id, out errorText);
+                TempData["Error"] = errorText;
 
-            ViewBag.IsUpdate = true;
-            return View("CreateOrEdit", model);
+                ViewBag.IsUpdate = true;
+                return View("CreateOrEdit", model);
+            }
+            else {
+                TempData["Message"] = "There was no item ID provided for editing, assuming creation of new item.";
+
+                ViewBag.IsUpdate = false;
+                return View("CreateOrEdit");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreateOrEdit(Card model, bool isUpdate = false) //[Bind(Include = "Id, Name")] 
+        [ActionName("Create")]
+        public ActionResult Insert(Card model, bool isUpdate = false) //[Bind(Include = "Id, Name")] 
+        {
+            return InsertOrUpdate(model, isUpdate);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Edit")]
+        public ActionResult Update(Card model, bool isUpdate = false) //[Bind(Include = "Id, Name")] 
+        {
+            return InsertOrUpdate(model, isUpdate);
+        }
+
+        private ActionResult InsertOrUpdate(Card model, bool isUpdate = false)
         {
             if (ModelState.IsValid)
             {
