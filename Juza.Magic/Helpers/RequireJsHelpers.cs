14d1bd8e3fcf51477ea680f70ac60b933cf3dd58
Justyna <justyna.heinbockel@penna.com>
2016-04-16 09:38:07 +0100

    Added and set up requirejs

---
+++ Juza.Magic/Helpers/RequireJsHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace Juza.Magic.Helpers
+{
+    public static class RequireJsHelpers
+    {
+        public static MvcHtmlString Require(this HtmlHelper helper, string config, string module)
+        {
+            var scriptsPath = "/Scripts/";
+            var requireConfigFile = "requireConfig.js";
+
+            var isValidModule = File.Exists(
+                helper.ViewContext.HttpContext.Server.MapPath(Path.Combine(scriptsPath, module + ".js")));
+
+            var requireScript = isValidModule
+                    ? string.Format(@"require([""{0}{1}"" ], function() {{
+                                        require([ ""{2}"" ]);
+                                    }});",
+                                    scriptsPath,
+                                    requireConfigFile,
+                                    module)
+                    : string.Empty;
+
+            return new MvcHtmlString(requireScript);
+        }
+
+        public static MvcHtmlString RequireScriptsForCurrentRoute(this HtmlHelper helper)
+        {
+            var action = helper.ViewContext.RouteData.Values["action"];
+            var controller = helper.ViewContext.RouteData.Values["controller"];
+
+            return helper.Require("config.js", string.Format("views/{0}/{1}", controller, action));
+        }
+    }
+}
---
+++ Juza.Magic/Scripts/requireConfig.js
@@ -0,0 +1,14 @@
+﻿require.config({
+    baseUrl: "/Scripts/",
+    paths: {
+        jquery: "jquery-2.2.3",
+        jqueryValidate: "jquery.validate",
+        jqueryValidateUnobtrusive: "jquery.validate.unobtrusive",
+        bootstrap: "bootstrap",
+        moment: "moment"
+    },
+    shim: {
+        jqueryValidate: ["jquery"],
+        jqueryValidateUnobtrusive: ["jquery", "jqueryValidate"]
+    }
+});
