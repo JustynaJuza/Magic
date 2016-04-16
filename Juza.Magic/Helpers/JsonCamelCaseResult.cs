90f7842542117e24b4d871d0fd594695da3238d4
Justyna <justyna.heinbockel@penna.com>
2016-04-16 09:39:10 +0100

    Added files from old project

---
+++ Juza.Magic/Helpers/JsonCamelCaseResult.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Magic.Helpers
+{
+    public class JsonCamelCaseResult : JsonResult
+    {
+        public static JsonCamelCaseResult Error(string error)
+        {
+            return new JsonCamelCaseResult(new { success = false, error }, JsonRequestBehavior.DenyGet, HttpStatusCode.InternalServerError);
+        }
+
+        public JsonCamelCaseResult(object data = null, JsonRequestBehavior jsonRequestBehavior = JsonRequestBehavior.AllowGet, HttpStatusCode responseStatusCode = HttpStatusCode.OK)
+        {
+            Data = data ?? new { };
+            JsonRequestBehavior = jsonRequestBehavior;
+            ResponseStatusCode = responseStatusCode;
+        }
+
+        public HttpStatusCode ResponseStatusCode { get; set; }
+
+        public string ResponseMessage { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (JsonRequestBehavior == JsonRequestBehavior.DenyGet && string.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.");
+            }
+
+            var response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(ContentType) ? ContentType : "application/json";
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+            if (Data == null)
+                return;
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            response.Write(JsonConvert.SerializeObject(Data, jsonSerializerSettings));
+            if (!string.IsNullOrWhiteSpace(ResponseMessage))
+            {
+                response.AddHeader("Message", ResponseMessage);
+            }
+            response.SetStatus(ResponseStatusCode);
+        }
+    }
+}
---
+++ Juza.Magic/Views/Shared/_TableControlsPartial.ascx
@@ -0,0 +1,13 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<Tuple<string, int, bool>>" %>
+
+<%: Html.ActionLink("View", "Details", Model.Item1, new { Area = "", id = Model.Item2 }, new { @class="btn btn-info", target="_blank"})%>
+
+<%: Html.ActionLink("Edit", "Edit", new { id = Model.Item2 }, new { @class="btn btn-warning" })%>
+
+<%: Html.ActionLink("Delete", "PreviewDelete", new { id = Model.Item2 }, new { @class="btn btn-danger" })%> 
+
+<% if (Model.Item3) { %>
+    <%: Html.ActionLink("Unpublish", "Publish", new { id = Model.Item2, publish = false }, new { @class="btn btn-inverse" }) %>
+<% } else { %> 
+    <%: Html.ActionLink("Publish", "Publish", new { id = Model.Item2 }, new { @class="btn btn-inverse" }) %>
+<% } %>
