834f099d00d908ccd0ee1582b7c249fcaf1a02a2
Justyna <justyna.heinbockel@gmail.com>
2016-07-03 23:08:34 +0100

    Fixed conditional requirejs helper

--- Juza.Magic/Helpers/RequireJsHelpers.cs
+++ Juza.Magic/Helpers/RequireJsHelpers.cs
@@ -15,6 +15,11 @@ namespace Juza.Magic.Helpers
                 throw new ArgumentException("No parameters passed. This function should be called with at least one parameter.");
             }
 
+            foreach (var module in modules)
+            {
+                helper.ViewContext.Controller.ViewData.Add("require-" + module, true);
+            }
+
             var requireScript = string.Format(
                 @"require(['{0}{1}'], function() {{
                       require(['{2}']);
@@ -28,20 +33,26 @@ namespace Juza.Magic.Helpers
 
         public static MvcHtmlString RequireConditionalScriptModule(this HtmlHelper helper, string module, string dependency)
         {
-            var requireScript = string.Format(
-                @"if (require.specified('{2}')) {{
-                      require(['{0}{1}'], function() {{
+            var isDependencyDefined = helper.ViewContext.Controller.ViewData.ContainsKey("require-" + dependency);
+
+            if (isDependencyDefined)
+            {
+                var requireScript = string.Format(
+                    @"require(['{0}{1}'], function() {{
                           require(['{2}'], function () {{
                               require(['{3}']);
                           }});
                       }});
-                  }}",
-                scriptsPath,
-                requireConfigFile,
-                dependency,
-                module);
+                    ",
+                    scriptsPath,
+                    requireConfigFile,
+                    dependency,
+                    module);
 
-            return new MvcHtmlString(requireScript);
+                return new MvcHtmlString(requireScript);
+            }
+
+            return new MvcHtmlString("");
         }
 
         public static MvcHtmlString RequireScriptsForCurrentRoute(this HtmlHelper helper)
--- Juza.Magic/Scripts/global/hub-start.js
+++ Juza.Magic/Scripts/global/hub-start.js
@@ -1,17 +1,16 @@
 ﻿// Start the hub connection if it's not running already.
 if ($.connection.hub && $.connection.hub.state === 4) {
     var hasChat = typeof chat != 'undefined';
-    // If hub handling provided by previously loaded hub script, call initialization functions.
-    if (hasChat) {
-        chat.hub = $.connection.chatHub;
-        chat.registerClientCallbacks(chat.hub.client);
-    }
+    var isGame = typeof game != 'undefined';
 
     $.connection.hub.start().done(function () {
         if (hasChat) {
+            // If hub handling provided by previously loaded hub script, call initialization functions.
+            chat.hub = $.connection.chatHub;
+            chat.registerClientCallbacks(chat.hub.client);
             chat.initialize();
         }
-        if (typeof game != 'undefined') {
+        if (isGame) {
             game.hub = $.connection.gameHub;
             game.initialized = true;
             game.initialize();
